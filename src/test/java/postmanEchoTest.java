7359a67e85f0ff45a384c523e4d6012d6160faef
Oleg Somov <doremi@fasollasi.ru>
2024-08-07 18:16:05 +0300

    Initial commit

---
+++ src/test/java/postmanEchoTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+class postmanEchoTest {
+    @Test
+    void test() {
+        var text = "Hi";
+        given()
+                .baseUri ("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"))
+                ;
+    }
+}
